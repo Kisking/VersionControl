d0eb75053f57910ae6e40cf7693e3b98a3a5f0c9
Kinga Kisfaludy <kinga.kisfaludy@stud.uni-corvinus.hu>
2023-04-10 20:59:30 +0200

    commit

---
+++ gyár/gyár/Entities/BallFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyár.Entities
+{
+    public class BallFactory : IToyFactory
+    {
+        public Toy CreateNew()
+        {
+            return new Ball();
+        }
+
+    }
+}
